a6e0ddc9f731f9fcd3f37b8c0664f32c76ea67a3
Ling Jiang <jiangling9981@gmail.com>
2023-05-31 16:36:32 +0100

    modified:   .DS_Store 	deleted:    .gitattributes 	renamed:    index1.html -> index.html 	modified:   src/app.js 	renamed:    src/styles1.css -> src/styles.css

---
+++ src/app.js
@@ -0,0 +1,56 @@
+let apiKey = "0c5f59b841b794d99933757d256233b1";
+
+function showWeatherDetails(response) {
+  console.log(response);
+  let temperature = Math.round(response.data.main.temp);
+  let humidity = response.data.main.humidity;
+  let wind = response.data.wind.speed;
+  let visibility = response.data.visibility;
+  let description = response.data.weather[0].description;
+  let currentTime = new Date();
+
+  let temperatureElement = document.getElementById("number");
+  let humidityElement = document.getElementById("humidity");
+  let windElement = document.getElementById("wind");
+  let visibilityElement = document.getElementById("visibility");
+  let descriptionElement = document.getElementById("description");
+  let currentTimeElement = document.getElementById("current-time");
+
+  temperatureElement.innerHTML = `${temperature}`;
+  humidityElement.innerHTML = `${humidity}`;
+  windElement.innerHTML = `${wind}`;
+  visibilityElement.innerHTML = `${visibility}`;
+  descriptionElement.innerHTML = `${description}`;
+  currentTimeElement.innerHTML = `${currentTime}`;
+}
+
+// show location
+
+function showPosition(position) {
+  let = latitude = position.coords.latitude;
+  let = longitude = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0c5f59b841b794d99933757d256233b1&units=metric`;
+
+  axios
+    .get(url)
+    .then((response) => {
+      showCityName(response);
+      showWeatherDetails(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showCityName(response) {
+  let cityName = response.data.name;
+  let cityNameElement = document.getElementById("city");
+  cityNameElement.innerHTML = cityName;
+}
+
+let currentButton = document.getElementById("current-button");
+currentButton.addEventListener("click", getCurrentPosition);
